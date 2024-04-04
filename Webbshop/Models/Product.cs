0172eceefa0c2bb4a40c033b880dea9f5c900a07
veronicasteen <veronica.steen@outlook.com>
2024-04-04 13:18:12 +0200

    hej

---
+++ Webbshop/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Webbshop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImagePath { get; set; }
+        public int AccountID { get; set; }
+        public Account Account { get; set; }
+    }
+}
