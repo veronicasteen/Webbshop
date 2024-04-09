90fcb3a4ec753371736b73badd34cfcbc9abda73
veronicasteen <veronica.steen@outlook.com>
2024-04-09 14:54:40 +0200

    Details-page

--- Webbshop/Pages/Details.cshtml.cs
+++ Webbshop/Pages/Details.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Webbshop.Data;
+using Webbshop.Models;
 
 namespace Webbshop.Pages
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+
+        private readonly AppDbContext _context;
+
+        public DetailsModel(AppDbContext context)
         {
+            _context = context;
+        }
+
+        public Product Product { get; set; }
+
+        public void OnGet(int id)
+        {
+            Product = _context.Products.Find(id);
+
         }
     }
 }
