87de66e7c4431546557a8604536be4587aeafd9e
veronicasteen <veronica.steen@outlook.com>
2024-04-09 09:51:12 +0200

    Added all razorpages

---
+++ Webbshop/Pages/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Webbshop.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
