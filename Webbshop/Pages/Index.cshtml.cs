e184da63f39f6587e6f05797d61afc2ac322ce5a
veronicasteen <veronica.steen@outlook.com>
2024-04-15 15:27:21 +0200

    Fixed 10 products by page

--- Webbshop/Pages/Index.cshtml.cs
+++ Webbshop/Pages/Index.cshtml.cs
@@ -17,9 +17,29 @@ namespace Webbshop.Pages
             this.database = database;
         }
 
-        public void OnGet()
+        public void OnGet(int pageNumber, string searchItem, string category)
         {
+
+			var pageSize = 10;
+
             Products = database.Products.OrderBy(p => p.Price).ToList();
-        }
+			
+			if (!string.IsNullOrEmpty(searchItem))
+			{
+				// Filtrera produkter baserat på söktermen
+				Products = Products.Where(p => p.Name.ToLower().Contains(searchItem)).ToList();
+			}
+
+			// Kontrollera om det finns en vald kategori
+			if (!string.IsNullOrEmpty(category) && category != "Any category")
+			{
+				// Filtrera produkter baserat på kategorin
+				Products = Products.Where(p => p.Category.ToLower() == category.ToLower()).ToList();
+			}
+
+			Products = Products.Skip((pageNumber -1 ) * pageSize)
+				.Take(pageSize).ToList();
+			
+		}
     }
 }
