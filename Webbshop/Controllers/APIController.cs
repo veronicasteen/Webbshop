8ec286f04df62aed449290e1475c78777fb02bf6
veronicasteen <veronica.steen@outlook.com>
2024-04-16 22:34:44 +0200

    api

--- Webbshop/Controllers/APIController.cs
+++ Webbshop/Controllers/APIController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Webbshop.Data;
+using Webbshop.Models;
 
 namespace Webbshop.Controllers
 {
@@ -9,11 +10,39 @@ namespace Webbshop.Controllers
     [ApiController]
     public class APIController : ControllerBase
     {
-        private readonly AppDbContext database;
+		private readonly AppDbContext database;
 
-        public APIController(AppDbContext database)
+		public APIController(AppDbContext database)
         {
             this.database = database;
         }
-    }
+
+		[HttpGet]
+        public IActionResult GetProducts(int pageNumber, string searchItem, string category)
+		{
+			var pageSize = 10;
+
+			var products = database.Products.OrderBy(p => p.Price).ToList();
+
+			if (!string.IsNullOrEmpty(searchItem))
+			{
+				products = products.Where(p => p.Name.ToLower().Contains(searchItem.ToLower())).ToList();
+			}
+
+			if (!string.IsNullOrEmpty(category) && category != "Any category")
+			{
+				products = products.Where(p => p.Category.ToLower() == category.ToLower()).ToList();
+			}
+
+			products = products.Skip((pageNumber - 1) * pageSize)
+							   .Take(pageSize)
+							   .ToList();
+
+
+
+			return Ok(products);
+
+		}
+
+	}
 }
