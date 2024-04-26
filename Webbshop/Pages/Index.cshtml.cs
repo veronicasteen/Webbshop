11348a06ae55c764c43a2679a10d8109928a7b70
veronicasteen <veronica.steen@outlook.com>
2024-04-26 12:32:05 +0200

    Fixed bugg

--- Webbshop/Pages/Index.cshtml.cs
+++ Webbshop/Pages/Index.cshtml.cs
@@ -2,31 +2,32 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Webbshop.Data;
 using Webbshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Webbshop.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly AppDbContext database;
+	public class IndexModel : PageModel
+	{
+		private readonly AppDbContext _database;
 
-        public List <Product> Products { get; set; }
-
-		public int PageNumber { get; set; } 
-
+		public List<Product> Products { get; set; }
+		public int PageNumber { get; set; }
 		public int TotalPages { get; set; }
+		public string Category { get; set; } 
+		public IndexModel(AppDbContext database)
+		{
+			_database = database;
+		}
 
-        public IndexModel(AppDbContext database)
-        {
-            this.database = database;
-        }
-
-        public void OnGet(int pageNumber, string searchItem, string category)
-        {
-			PageNumber= pageNumber;
+		public void OnGet(int pageNumber, string searchItem, string category)
+		{
+			PageNumber = pageNumber;
 			var pageSize = 10;
 
-            Products = database.Products.OrderBy(p => p.Price).ToList();
-			
+			Products = _database.Products.OrderBy(p => p.Price).ToList();
+
 			if (!string.IsNullOrEmpty(searchItem))
 			{
 				Products = Products.Where(p => p.Name.ToLower().Contains(searchItem)).ToList();
@@ -34,22 +35,21 @@ namespace Webbshop.Pages
 
 			if (!string.IsNullOrEmpty(category) && category != "Any category")
 			{
-				Products = Products.Where(p => p.Category.ToLower() == category.ToLower()).ToList();
+				Category = category.ToLower(); 
+				Products = Products.Where(p => p.Category.ToLower() == Category).ToList();
 			}
 
-            int totalProducts = Products.Count; 
-            TotalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
-            PageNumber = Math.Clamp(PageNumber, 1, TotalPages);
+			int totalProducts = Products.Count;
+			TotalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
+			PageNumber = Math.Clamp(PageNumber, 1, TotalPages);
 
-            //Här sätts första sidan till 1 även om använderen inte gjort en sökning än
-            if (PageNumber < 1)
-            {
-                PageNumber = 1;
-            }
+			if (PageNumber < 1)
+			{
+				PageNumber = 1;
+			}
 
-            Products = Products.Skip((pageNumber -1 ) * pageSize)
+			Products = Products.Skip((pageNumber - 1) * pageSize)
 				.Take(pageSize).ToList();
-			
 		}
-    }
+	}
 }
